ad85b8b4f38da45aade3e0451d6d45d1229570e8
suwandarathnap-code <suwandarathnap@gmail.com>
2026-04-17 11:30:17 +0530

    lab04 q4 final policy class

--- Lab04Main.java
+++ Lab04Main.java
@@ -1,10 +1,13 @@
 public class Lab04Main {
     public static void main(String[] args) {
 
+        UniversityPolicy.showPolicyHeader();
+        System.out.println(UniversityPolicy.UNIVERSITY_NAME);
+
         Lecturer l = new Lecturer("Roshan",101,"IT",3,50000);
         LabAssistant a = new LabAssistant("Sandun",201,"IT",100,500);
 
-         l.displayLecturerDetails();
-         a.displayLabAssistantDetails();
+        System.out.println(UniversityPolicy.calculateBonus(l.calculateMonthlyPayment()));
+        
     }
 }
