3f6a0ec623de318794981197a59507c649c8a7f2
suwandarathnap-code <suwandarathnap@gmail.com>
2026-04-17 11:09:37 +0530

    lab04 q2 lecturer child class

--- Lab04Main.java
+++ Lab04Main.java
@@ -1,7 +1,7 @@
 public class Lab04Main {
     public static void main(String[] args) {
 
-        Lecturer l = new Lecturer("Kamal",101,"IT",3,50000);
+        Lecturer l = new Lecturer("Roshan",101,"IT",3,50000);
 
         l.displayLecturerDetails();
         System.out.println(l.calculateMonthlyPayment());
